93e20db3cd0a74de159457e26c54939a3fe3a870
Vinicius Leao <vinicius.s.silva-kee@parceirosedu.com.br>
2023-02-09 12:23:22 -0300

    initial commit

---
+++ src/test/java/br/com/valloo/cards/CardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valloo.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
